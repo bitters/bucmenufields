b371edb3bd0f9f69109fd0cac9372949bc012c56
brnhrst <michael@bitters.de>
2015-05-13 12:07:36 +0200

    Initial Commit

---
+++ bucmenufields.php
@@ -0,0 +1,28 @@
+<?php
+// no direct access
+defined ( '_JEXEC' ) or die ( 'Restricted access' );
+class plgContentBucmenufields extends JPlugin {
+	/**
+	 * Load the language file on instantiation.
+	 * Note this is only available in Joomla 3.1 and higher.
+	 * If you want to support 3.0 series you must override the constructor
+	 *
+	 * @var boolean
+	 * @since 3.1
+	 */
+	protected $autoloadLanguage = true;
+	function onContentPrepareForm($form, $data) {
+		$app = JFactory::getApplication();
+		$option = $app->input->get('option');
+		switch($option) {
+			case 'com_menus':
+				if ($app->isAdmin()) {
+					JForm::addFormPath(__DIR__ . '/forms');
+					$form->loadFile('menufields', false);
+				}
+				return true;
+		}
+		return true;
+	}
+}
+?>
